1d31dbf4c89094ca7c1d59925c490a18eef23624
atenyaeves <atenyaev@protonmail.com>
2021-12-07 20:09:20 +0700

    Added ui test

--- app/src/androidTest/java/com/example/innopolisarterialpressure/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/innopolisarterialpressure/ExampleInstrumentedTest.kt
@@ -1,5 +1,11 @@
 package com.example.innopolisarterialpressure
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
@@ -7,6 +13,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +22,26 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @get:Rule
+    var activityScenarioRule = activityScenarioRule<MainActivity>()
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.innopolisarterialpressure", appContext.packageName)
+    }
+
+    @Test
+    fun onButtonAddClickTest() {
+        onView(withId(R.id.apDetailsCreateButton)).perform(click())
+        onView(withId(R.id.apDetailsSaveButton)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun onScreenInputTest() {
+        onView(withId(R.id.apDetailsCreateButton)).perform(click())
+        onView(withId(R.id.apDetailsMorningText)).check(matches(isDisplayed()))
     }
 }
